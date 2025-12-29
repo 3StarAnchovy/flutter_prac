4f785651cc26096c2261a509e15784245a172920
홍지민 <baker0929@naver.com>
2025-12-29 13:21:25 +0900

    feat : 상단 Appbar 배치

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: Text('data'));
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        /* 아이폰은 가운데가 디폴트, 안드는 왼쪽임*/
+        title: Text("Instagram"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add_box_outlined)),
+        ],
+      ),
+      body: Text('data'),
+    );
   }
 }
